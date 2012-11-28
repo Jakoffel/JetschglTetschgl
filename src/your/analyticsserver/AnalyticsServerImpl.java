9124a2c830118a06aeb6f860bc76b119a4808a71
MichaelJosef <MichaelJosef@128.131.210.29>
2012-11-28 14:34:53 +0100

    perfekt gemacht :)

--- src/your/analyticsserver/AnalyticsServerImpl.java
+++ src/your/analyticsserver/AnalyticsServerImpl.java
@@ -2,20 +2,27 @@ package your.analyticsserver;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import your.common.rmi.AnalyticsServer;
 import your.common.rmi.NotificationCallback;
 import your.common.rmi.RmiHostPort;
 import your.common.rmi.events.Event;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AnalyticsServerImpl implements AnalyticsServer {
 	
 	private RmiHostPort rhp;
 	private ArrayList<Event> eventList = new ArrayList<Event>();
+	private List<ManagementUser> managementUserList;
+	
 	
 	public AnalyticsServerImpl() {
 		rhp = new RmiHostPort();
+		managementUserList = Collections.synchronizedList(new ArrayList<ManagementUser>());
+		
 	}
 
 	public int getRmiPort() {
---
+++ src/your/analyticsserver/ManagementUser.java
@@ -0,0 +1,42 @@
+package your.analyticsserver;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import your.common.rmi.NotificationCallback;
+
+public class ManagementUser {
+	
+	private static AtomicInteger idCounter = new AtomicInteger(0);
+	private int userID;
+	private NotificationCallback callback;
+	private String filter;
+	
+	public ManagementUser(NotificationCallback callback,
+			String filter) {
+
+		this.callback = callback;
+		this.filter = filter;
+		assignNewUserID();
+	}
+
+	public int getUserId() {
+		return userID;
+	}
+
+	public NotificationCallback getCallback() {
+		return callback;
+	}
+
+	public String getFilter() {
+		return filter;
+	}
+	
+	private void assignNewUserID() {
+		userID = idCounter.incrementAndGet();
+	}
+	
+	
+	
+	
+	
+}
